bc939cf9d332d1f9e3c16a43f1fc785f3a8998e3
Unchaptered <workstation19961002@gmail.com>
2022-07-30 00:23:40 +0900

    Update `**/jwt.provider.js`, 토큰 공급자 함수

--- src/modules/providers/jwt.provider.js
+++ src/modules/providers/jwt.provider.js
@@ -1,18 +1,59 @@
 import jwt from "jsonwebtoken";
 
+
 class JwtProvider {
 
     static SECRET;
+    static JWT_ALGORITHM;
 
     constructor() {}
 
-    static initialize(JWT_SECRET) {
+    static initialize(JWT_SECRET, JWT_ALGORITHM) {
 
         this.SECRET = JWT_SECRET;
+        this.JWT_ALGORITHM = JWT_ALGORITHM;
         
     }
+
+    sign(payload) {
+
+        return jwt.sign(payload, JwtProvider.SECRET, {
+            algorithm: JwtProvider.JWT_ALGORITHM
+        });
+
+    }
+
+    /**
+     * JWT 를 분해합니다..
+     * 유효한 토큰인지는 확인하며, 유효하지 않을 시 에러가 발생됩니다.
+     * 
+     * @param {*} token 
+     * @returns payload: Object
+     * @throws `JsonWebTokenError`: invalid signature
+     */
+    verifyToken(token) {
+
+        return jwt.verify(token, JwtProvider.SECRET);
+
+    }
+
+    /**
+     * JWT 를 분해합니다.
+     * 유효한 토큰인지는 확인하지 않습니다.
+     * 
+     * @param {*} token 
+     * @returns payload: Object
+     */
+    decodeToken(token) {
+
+        return jwt.decode(token);
+
+    }
+
+
     
 
 }
+
 
 export default JwtProvider;
