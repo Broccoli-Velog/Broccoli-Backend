5d60d52d2d146214451f71749325a05f155de412
Unchaptered <workstation19961002@gmail.com>
2022-08-03 01:48:54 +0900

    Update `**/auth.js`

--- src/routes/controllers/auth.js
+++ src/routes/controllers/auth.js
@@ -1,10 +1,8 @@
 import Joi from "joi";
 
-import Jwt from 'jsonwebtoken';
 import { JwtProvider, BcryptProvider, DatabaseProvider } from "../../modules/_.loader.js";
 import { UserJoi, UserModel } from "../../models/_.loader.js";
 
-import db from "../../database/index.js";
 import * as utils from "../../modules/utils.js";
 
 const register = async (req, res, next) => {
@@ -18,7 +16,6 @@ const register = async (req, res, next) => {
             email: UserJoi.email.required(),
             nickname: UserJoi.nickname.required(),
             password: UserJoi.password.required()
-
         }).validateAsync({ email, nickname, password });
         
         const bcryptProvider = new BcryptProvider();
@@ -31,19 +28,21 @@ const register = async (req, res, next) => {
                                     ELSE true
                                 END) as is_exists
                             FROM broccoli.user
-                            WHERE email = '${email}' OR nickname = '${nickname}';`;
+                            WHERE email = "${email}" OR nickname = "${nickname}";`;
 
-        const [ [ { is_exists } ] ] = await db.query(IS_EXISTS);
+        const connection = await new DatabaseProvider().getConnection();
+
+        const [ [ { is_exists } ] ] = await connection.query(IS_EXISTS);
         if (Boolean(is_exists)) {
 
-            return res.status(403).json(utils.createJson(false, '이메일 및 닉네임이 중복된 사용자가 존재합니다.', {}));
+            return res.status(403).json(utils.createJson(false, "이메일 및 닉네임이 중복된 사용자가 존재합니다.", {}));
 
         } else {
 
-            const INSERT = `INSERT INTO user (email, nickname, password) VALUES ('${email}', '${nickname}', '${hashedPassword}');`;
+            const INSERT = `INSERT INTO user (email, nickname, password) VALUES ("${email}", "${nickname}", "${hashedPassword}");`;
 
-            const [ ResultSetHeader ] = await db.query(INSERT);
-            return res.status(201).json(utils.createJson(true, '회원가입에 성공하였습니다.', {
+            const [ ResultSetHeader ] = await connection.query(INSERT);
+            return res.status(201).json(utils.createJson(true, "회원가입에 성공하였습니다.", {
                 email: userModel.email,
                 nickname: userModel.nickname
             }));
@@ -71,21 +70,23 @@ const login = async (req, res, next) => {
 
         }).validateAsync({ email, password });
 
-        const FIND = `SELECT nickname, password FROM user WHERE email = '${email}' LIMIT 1;`;
+        const FIND = `SELECT nickname, password FROM user WHERE email = "${email}" LIMIT 1;`;
+        
+        const connection = await new DatabaseProvider().getConnection();
 
-        const [ [ Result ] ] = await db.query(FIND);
+        const [ [ Result ] ] = await connection.query(FIND);
         if (!Result)
-            return res.status(404).json(utils.createJson(false, '존해하지 않는 사용자입니다.', { email }));
+            return res.status(404).json(utils.createJson(false, "존해하지 않는 사용자입니다.", { email }));
 
         const bcryptProvider = new BcryptProvider();
         const isCorrectPassword = await bcryptProvider.isCorrectPassword(password, Result.password);
         if (!isCorrectPassword)
-            return res.status(400).json(utils.createJson(false, '비밀번호가 일치하지 않습니다.', { email }));
+            return res.status(400).json(utils.createJson(false, "비밀번호가 일치하지 않습니다.", { email }));
         
         const jwtProvider = new JwtProvider();
         const token = jwtProvider.sign({ nickname: Result.nickname });
 
-        return res.status(201).json(utils.createJson(true, '로그인에 성공하셨습니다.', { email, nickname: Result.nickname }, token ));
+        return res.status(201).json(utils.createJson(true, "로그인에 성공하셨습니다.", { email, nickname: Result.nickname }, token ));
 
     } catch(err) {
 
