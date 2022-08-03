7dc65565896b2732a3c1bcc5192a871f172e2139
Unchaptered <workstation19961002@gmail.com>
2022-08-03 17:07:08 +0900

    Create `**/controllers/user.js`

---
+++ src/routes/controllers/user.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+import { DatabaseProvider  } from "../../modules/_.loader";
+
+import * as utils from "../../modules/utils.js";
+
+const getUserNotes = (req, res, next) => {
+};
+
+
+export {
+
+    getUserNotes,
+    
+}
--- src/routes/routers/user.js
+++ src/routes/routers/user.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
 
+import * as userController from "../controllers/user.js";
+
 const userRouter = Router();
+
+userRouter.route("/:nickname")
+    .get(userController.getUserNotes);
 
 export default userRouter;
