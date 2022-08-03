0b604e03f8edc9bc3c91d116cf70ab39b8c38663
Unchaptered <workstation19961002@gmail.com>
2022-08-03 23:02:10 +0900

    Update `**/(controllers|routers)/auth.js`, 내 정보 보기/수정/탈퇴 구현

--- src/routes/routers/auth.js
+++ src/routes/routers/auth.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
+import { tokenGuard } from "../../modules/_.loader.js";
 import { AuthController } from "../controllers/_.export.js";
 
 const authRouter = Router();
 
+authRouter.route("")
+    .get(tokenGuard, AuthController.getProfile)
+    .patch(tokenGuard, AuthController.patchAuth)
+    .delete(tokenGuard, AuthController.deleteAuth);
+    
 authRouter.route("/register")
     .post(AuthController.register);
 
