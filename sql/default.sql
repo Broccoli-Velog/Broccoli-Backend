8789ed4215f845f604bac06ea933671493a1e5fc
Unchaptered <workstation19961002@gmail.com>
2022-08-05 22:46:40 +0900

    Fix `sql/**`

--- sql/default.sql
+++ sql/default.sql
@@ -3,11 +3,7 @@ DROP DATABASE IF EXISTS broccoli;
 CREATE DATABASE IF NOT EXISTS broccoli;
 USE broccoli;
 
-DROP TABLE IF EXISTS comment;
-DROP TABLE IF EXISTS note;
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE user (
     user_id     SERIAL          NOT NULL    PRIMARY KEY,
     email       VARCHAR(40)     NOT NULL,
     nickname    VARCHAR(10)     NOT NULL,
@@ -16,32 +12,28 @@ CREATE TABLE IF NOT EXISTS user (
     updated_at   DATETIME       NOT NULL    DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS note (
+CREATE TABLE note (
     note_id     SERIAL          NOT NULL    PRIMARY KEY,
-    fk_user_id  INTEGER         NOT NULL,
+    fk_user_id  INTEGER         NOT NULL    REFERENCES user(user_id),
     title       VARCHAR(50)     NOT NULL,
     content     VARCHAR(255)    NOT NULL,
     image       VARCHAR(200),
     series      VARCHAR(20),
     created_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP
 
     -- 기본키 걸기
     -- PIMARY KEY note_id
 
     -- 외래키 걸기
-    FOREIGN KEY (fk_user_id) REFERENCES user (user_id)
-    
+    -- FOREIGN KEY fk_user_id REFERENCES ON use (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS comment (
+CREATE TABLE comment (
     comment_id  SERIAL          NOT NULL    PRIMARY KEY,
     content     VARCHAR(100)    NOT NULL,
-    fk_note_id  INTEGER         NOT NULL,
-    fk_user_id  INTEGER         NOT NULL,
+    fk_note_id  INTEGER         NOT NULL    REFERENCES note(note_id),
+    fk_user_id  INTEGER         NOT NULL    REFERENCES user(user_id),
     created_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    
-    FOREIGN KEY (fk_user_id) REFERENCES note (note_id),
-    FOREIGN KEY (fk_user_id) REFERENCES user (user_id)
+    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP
 );
