1b3bd957ee46e82193fe396e4e39a2bb33146ff6
Unchaptered <workstation19961002@gmail.com>
2022-08-05 22:24:21 +0900

    Fix `default.sql`

--- sql/backup.sql
+++ sql/backup.sql
@@ -14,30 +14,26 @@ CREATE TABLE user (
 
 CREATE TABLE note (
     note_id     SERIAL          NOT NULL    PRIMARY KEY,
-    fk_user_id  INTEGER         NOT NULL,
+    fk_user_id  INTEGER         NOT NULL    REFERENCES user(user_id),
     title       VARCHAR(50)     NOT NULL,
     content     VARCHAR(255)    NOT NULL,
     image       VARCHAR(200),
     series      VARCHAR(20),
     created_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP
 
     -- 기본키 걸기
     -- PIMARY KEY note_id
 
     -- 외래키 걸기
-    FOREIGN KEY (fk_user_id) REFERENCES user (user_id)
-    
+    -- FOREIGN KEY fk_user_id REFERENCES ON use (user_id)
 );
 
 CREATE TABLE comment (
     comment_id  SERIAL          NOT NULL    PRIMARY KEY,
     content     VARCHAR(100)    NOT NULL,
-    fk_note_id  INTEGER         NOT NULL,
-    fk_user_id  INTEGER         NOT NULL,
+    fk_note_id  INTEGER         NOT NULL    REFERENCES note(note_id),
+    fk_user_id  INTEGER         NOT NULL    REFERENCES user(user_id),
     created_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    
-    FOREIGN KEY (fk_user_id) REFERENCES note (note_id),
-    FOREIGN KEY (fk_user_id) REFERENCES user (user_id)
+    updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP
 );
